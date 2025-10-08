01570f4bb6ac48e4e131cb69d93175de0fee04a1
ichiharak-ux <ichihara_k@tokium.jp>
2025-10-08 09:13:49 +0000

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,27 @@
-class StaticPagesController < ApplicationController
-  def home
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get home' do
+    get root_path
+    assert_response :success
+    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
-  def help
+  test 'should get help' do
+    get help_path
+    assert_response :success
+    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'should get about' do
+    get about_path
+    assert_response :success
+    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'should get contact' do
+    get contact_path
+    assert_response :success
+    assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
   end
 end
